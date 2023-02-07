d41a8b4409da96ac70da13d1430c38e2f7f1cb17
Emanuel Cardoso Pereira <44120438+emanuel-pereira@users.noreply.github.com>
2023-02-07 15:23:00 +0000

    Correct mongo container image

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 const buildClient = ({ req }) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     // We are on the server
 
     return axios.create({
-      baseURL: "http://www.YOUR_DOMAIN_NAME",
+      baseURL: 'http://127.0.0.1:5500',
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: "/",
+      baseUrl: '/',
     });
   }
 };
